905c4142b696e56819c3d24d0e58ec2e89ca86b5
Venkat55 <110102761+VenkateswaraReddy5@users.noreply.github.com>
2023-03-02 19:50:45 +0530

    Update env.sh

--- deploy-apigee-proxy/env.sh
+++ deploy-apigee-proxy/env.sh
@@ -13,9 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export PROJECT="<GCP_PROJECT_ID>"
-export APIGEE_HOST="<APIGEE_DOMAIN_NAME>"
-export APIGEE_ENV="<APIGEE_ENVIRONMENT_NAME>"
+export PROJECT="apigedemo1"
+export APIGEE_HOST="34.111.167.153.nip.io"
+export APIGEE_ENV="eval"
 PROJECT_NUMBER="$(gcloud projects describe $PROJECT --format="value(projectNumber)")"
 export PROJECT_NUMBER
 export CLOUD_BUILD_SA="$PROJECT_NUMBER@cloudbuild.gserviceaccount.com"
